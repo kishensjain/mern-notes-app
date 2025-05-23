344de7509834adc2a2298f09238cfb0e4d664288
Kishen S Jain <jain.skishen@gmail.com>
2025-05-23 11:51:45 +0530

    add routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
+import {Routes, Route} from 'react-router'
+import HomePage from "./pages/HomePage"
+import CreatePage from "./pages/CreatePage"
+import NoteDetailPage from "./pages/NoteDetailPage"
+
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className='min-h-screen bg-blue-200 text-gray-900'>
+      <Routes>
+        <Route path = "/" element = {<HomePage/>} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
+      </Routes>
+    </div>
   )
 }
 
