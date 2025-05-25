a4d5a3c7967099acb6c687ce7ec521254a7791be
Kishen S Jain <jain.skishen@gmail.com>
2025-05-25 10:06:24 +0530

    connect database

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,13 +2,15 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import { connectDB } from "./config.js/mongoose.js";
+
+dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.listen(PORT, () => {
-  console.log("Server started on PORT:", PORT);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server started on port: ", PORT);
+  });
 });
