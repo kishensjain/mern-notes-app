da05adc97711bcd5c4289a03de1b4cadd61d4116
Kishen S Jain <jain.skishen@gmail.com>
2025-05-30 10:18:50 +0530

    add theme to local storage

--- frontend/src/context/ThemeContext.jsx
+++ frontend/src/context/ThemeContext.jsx
@@ -1,23 +1,28 @@
-import { createContext, useEffect, useState, useContext } from "react"
+import { createContext, useEffect, useState, useContext } from "react";
 
+const ThemeContext = createContext();
 
-const ThemeContext = createContext()
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState(() => {
+    const storedTheme = localStorage.getItem("theme");
+    return storedTheme === "dark" ? "dark" : "light";
+  });
 
-export const ThemeProvider = ({children}) =>{
-  const [theme, setTheme] = useState("light");
-  useEffect(()=>{
-    document.documentElement.classList.toggle("dark", theme === "dark")
-  },[theme])
-  const toggleTheme = () =>{
-    setTheme((prev)=>(prev === "light" ? "dark" : "light"))
-  }
-  return(
-    <ThemeContext.Provider value={{theme, toggleTheme}}>
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+    theme === "dark"
+      ? document.documentElement.classList.add("dark")
+      : document.documentElement.classList.remove("dark");
+  }, [theme]);
+
+  const toggleTheme = () =>
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
       {children}
     </ThemeContext.Provider>
-  )
-}
+  );
+};
 
-//Create a custom hook for using the theme context
-//This is a shortcut so other components can just call useTheme() instead of writing useContext(ThemeContext) every time.
 export const useTheme = () => useContext(ThemeContext);
